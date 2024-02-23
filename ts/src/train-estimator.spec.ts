0f04593482f5c6cdee5d264460a7e46417040e10
Mickael Brunet <mickael.brunet64@gmail.com>
2024-02-23 14:19:10 +0100

    add first test when no passengers

--- ts/src/train-estimator.spec.ts
+++ ts/src/train-estimator.spec.ts
@@ -1,6 +1,33 @@
+import { TripRequest } from "./model/trip.request";
+import { TrainTicketEstimator } from "./train-estimator";
 
 describe("train estimator", function () {
     it("should work", () => {
         expect(1 + 2).toBe(3);
     });
+});
+
+
+describe('create an object from TrainEstimator class', () => {
+
+})
+
+describe('TrainTicketEstimator', () => {
+    let estimator: TrainTicketEstimator;
+
+    beforeEach(() => {
+        estimator = new TrainTicketEstimator();
+    });
+
+    it('estimate returns 0 when no passengers', async () => {
+        const tripDetails: TripRequest = {
+            passengers: [],
+            details: { from: 'Paris', to: 'Lyon', when: new Date() }
+        };
+
+        const result = await estimator.estimate(tripDetails);
+
+        expect(result).toBe(0);
+    });
+
 });
